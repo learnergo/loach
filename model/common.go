03d28af5bc43034d9ff4c0ea656e8a200d6d0c0f
zhangxm <3201248483@qq.com>
2018-04-18 09:46:10 +0800

    update model structure

--- model/common.go
+++ model/common.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"fmt"
 )
 
@@ -12,68 +10,15 @@ type Response struct {
 	Messages []string      `json:"messages"`
 }
 
+type ResponseErr struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
 func (r Response) Error() error {
 	errors := ""
 	for _, e := range r.Errors {
 		errors += e.Message + ": "
 	}
 	return fmt.Errorf(errors)
-}
-
-type ResponseErr struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-type RegisterRequest struct {
-	EnrollID       string              `json:"id"`
-	Type           string              `json:"type"`
-	Secret         string              `json:"secret,omitempty"`
-	MaxEnrollments int                 `json:"max_enrollments,omitempty"`
-	Affiliation    string              `json:"affiliation"`
-	Attrs          []RegisterAttribute `json:"attrs"`
-	CAName         string              `json:"caname,omitempty"`
-}
-
-type RegisterAttribute struct {
-	Name  string `json:"name"`
-	Value string `json:"value"`
-	ECert bool   `json:"ecert,omitempty"`
-}
-
-type RegisterResponse struct {
-	Response
-	Result RegisterCredentialResponse `json:"result"`
-}
-
-type RegisterCredentialResponse struct {
-	Secret string `json:"secret"`
-}
-
-type EnrollRequest struct {
-	EnrollID string
-	Secret   string
-	Name     pkix.Name
-	Profile  string            `json:"profile,omitempty"`
-	Label    string            `json:"label,omitempty"`
-	CAName   string            `json:"caname,omitempty"`
-	Hosts    []string          `json:"hosts,omitempty"`
-	Attrs    []EnrollAttribute `json:"attr_reqs,omitempty"`
-}
-
-type EnrollAttribute struct {
-	Name     string `json:"name"`
-	Optional bool   `json:"optional,omitempty"`
-}
-
-type EnrollResponse struct {
-	Identity  *Identity
-	CertChain *x509.Certificate
-}
-
-type CreateCsrRequest struct {
-	Name      pkix.Name
-	Key       interface{}
-	Hosts     []string
-	Algorithm string
 }
